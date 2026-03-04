3f7853c748fbc1e4a77453cc1ec91c4cb3875881
eklavya777 <atomdeadshot955@gmail.com>
2026-03-04 23:23:31 +0530

    Fixed import issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/home";
+import Home from "./components/Home";
 import InputPage from "./components/inputPage";
 
 function App() {
