5e4c2b3c27fdf9c42216e474976a1090428faa18
eklavya777 <140241810+eklavya777@users.noreply.github.com>
2026-04-12 00:43:40 +0530

    Update userChoice.jsx

--- src/components/userChoice.jsx
+++ src/components/userChoice.jsx
@@ -70,7 +70,7 @@ function UserChoice() {
     try {
 
       const res = await axios.get(
-        `http://localhost:5000/get-attempts/${user.uid}`
+        `https://jlpt-score-calulator.onrender.com/get-attempts/${user.uid}`
       );
 
       const filteredAttempts = res.data.attempts.filter(
